432b1aafe4d0ee082f441bead3c7975dbb34e3f6
Amit Chauhan <your-email@gmail.com>
2026-03-07 10:22:24 +0530

    Removed extra programs (03-25)

--- programs/12_program.py
+++
@@ -1,9 +0,0 @@
-#  Write a Python Program to Check if a Number is Odd or Even.
-
-num = int(input("Enter the num: "))
-
-if num%2 == 0:
-    print("This is a even number: ")
-else:
-    print("This is a odd number: ")
-    
--- programs/17_program.py
+++
@@ -1,7 +0,0 @@
-# Write a Python Program to Display the multiplication Table.
-
-num = int(input("Display multiplication table of: "))
-
-for i in range(1, 11):
-    print(f"{num} X {i} = {num *i}")
-
